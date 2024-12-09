00e967ae244448544e41f0436fd8ef9e2e400e42
Antoine Allioux <antoine.allioux@gmail.com>
2024-12-09 15:32:18 +0100

    temporal cloud experiment

--- src/bin/worker.rs
+++ src/bin/worker.rs
@@ -1,5 +1,6 @@
 use std::{any::type_name_of_val, error::Error, str::FromStr, sync::Arc};
 
+use temporal_client::TlsConfig;
 use temporal_sdk::{self, sdk_client_options, Worker};
 use temporal_sdk_core::{init_worker, CoreRuntime, Url};
 use temporal_sdk_core_api::{telemetry::TelemetryOptionsBuilder, worker::WorkerConfigBuilder};
@@ -9,19 +10,28 @@ use lib::{
     workflow::money_transfer,
 };
 
-pub fn type_of<T>(_: T) -> &'static str {
-    std::any::type_name::<T>()
-}
-
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    let server_options = sdk_client_options(Url::from_str("http://localhost:7233")?).build()?;
-    let client = server_options.connect("default", None).await?;
+    let api_key = "";
+
+    let server_options =
+        sdk_client_options(Url::from_str("https://eu-west-2.aws.api.temporal.io:7233")?)
+            .api_key(Some(api_key.to_string()))
+            //.client_name("sugar-temporal-client")
+            //.client_name("transcoder-test")
+            //.identity("sugar temporal client".to_owned())
+            //.identity("transcoder-test".to_owned())
+            .tls_cfg(TlsConfig::default())
+            .build()?;
+
+    let client = server_options.connect("sugar-test.yyja2", None).await?;
+    /*
+
     let telemetry_options = TelemetryOptionsBuilder::default().build()?;
     let runtime = CoreRuntime::new_assume_tokio(telemetry_options)?;
 
     let worker_config = WorkerConfigBuilder::default()
-        .namespace("default")
+        //.namespace("sugar-test.yyja2")
         .task_queue("task_queue")
         .worker_build_id("rust-sdk")
         .build()?;
@@ -35,7 +45,7 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
     worker.register_wf(type_name_of_val(&money_transfer), money_transfer);
 
-    worker.run().await?;
+    worker.run().await?;*/
 
     Ok(())
 }
